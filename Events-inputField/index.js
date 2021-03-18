2e1a43fc61109ed8e8dce109f64a10d88a17ac35
nrcool <nrcoolove@yahoo.com>
2021-03-18 13:20:33 +0100

    event object (event.target and event.currentTarget)

--- Events-inputField/index.js
+++ Events-inputField/index.js
@@ -20,7 +20,7 @@ let count = 1;
 
 document.querySelector("h1").addEventListener("click", handler) */
 
-let ul = document.querySelector("ul")
+/* let ul = document.querySelector("ul") */
 
 //add event listener
 /*  ul.addEventListener("mouseenter", ()=>{
@@ -37,10 +37,23 @@ ul.addEventListener("mouseleave",()=>{
      ul.classList.toggle("nav")
  }) */
  
- let dropdown = document.querySelector(".dropdown")
+/*  let dropdown = document.querySelector(".dropdown")
 
- dropdown.addEventListener("click",()=>{
+ dropdown.addEventListener("click",(e)=>{
+        console.log(e)
         let i = document.querySelector("i")
         i.classList.toggle("nav") //remove that class
         ul.classList.toggle("nav") //add that class
+ }) */
+
+
+ let ul = document.querySelector("ul")
+
+ document.addEventListener("click",(event)=>{
+/*     console.log(event) */
+    console.log(event.target) //where user clicks
+   /*  event.target.style.backgroundColor="green" */
+ /*   ul.removeChild(event.target) */
+    console.log(event.currentTarget)  //element who is listening for that event 
+ //element to which you attached event listener
  })
