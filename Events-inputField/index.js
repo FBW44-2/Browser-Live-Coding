92c82d0836959ac1b1734a112ec664e8f26c47c6
nrcool <nrcoolove@yahoo.com>
2021-03-18 12:04:27 +0100

    added click and mouseleave and mouseeneter events

--- Events-inputField/index.js
+++ Events-inputField/index.js
@@ -1,6 +1,4 @@
-let inputs = document.querySelectorAll("input")//NodeList
-
-
+/* let inputs = document.querySelectorAll("input")//NodeList
 
 let btn = document.querySelector("button")
 let p =document.querySelector("p")
@@ -8,7 +6,6 @@ btn.addEventListener("click",()=>{
    console.dir(inputs) 
    p.innerText= `hi, ${inputs[0].value} ${inputs[1].value}`
 })
-
 
 
 let count = 1;
@@ -21,8 +18,29 @@ let count = 1;
     }
  }
 
-document.querySelector("h1").addEventListener("click", handler)
+document.querySelector("h1").addEventListener("click", handler) */
 
+let ul = document.querySelector("ul")
 
+//add event listener
+/*  ul.addEventListener("mouseenter", ()=>{
+     console.log("entered into UL")
+     ul.classList.add("nav")
+ })
+ 
+ul.addEventListener("mouseleave",()=>{
+    ul.classList.remove("nav")
+}) */
 
+/*   ul.addEventListener("click", ()=>{
+     console.log("entered into UL")
+     ul.classList.toggle("nav")
+ }) */
+ 
+ let dropdown = document.querySelector(".dropdown")
 
+ dropdown.addEventListener("click",()=>{
+        let i = document.querySelector("i")
+        i.classList.toggle("nav") //remove that class
+        ul.classList.toggle("nav") //add that class
+ })
