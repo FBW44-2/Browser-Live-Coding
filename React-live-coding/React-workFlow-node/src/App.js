6829adc52a460f4abe7da69c4288ff782dc23045
nrcool <nrcoolove@yahoo.com>
2021-04-12 13:41:15 +0200

    workflow-react

---
+++ React-live-coding/React-workFlow-node/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+/* const element = <h1>Hello world</h1> */
+function Hello(){
+    return(
+        <div>
+            <h1>Hello World </h1>
+            <h2>This is my First React App</h2>
+        </div>
+    )
+}
+
+export default Hello;
