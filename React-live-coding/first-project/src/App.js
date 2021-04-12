24f2b428784e2abc3f4a7aeba1cf53cf673805fe
nrcool <nrcoolove@yahoo.com>
2021-04-12 16:55:15 +0200

    cleanup react boilerplate code

---
+++ React-live-coding/first-project/src/App.js
@@ -0,0 +1,14 @@
+/* import React from "react" */
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, fugiat!
+      </p>
+    </div>
+  );
+}
+
+export default App;
