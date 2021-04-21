c182bdc2111bad001d3123e5564e368f76c62c4e
nrcool <nrcoolove@yahoo.com>
2021-04-21 10:30:58 +0200

    history props

--- React-live-coding/React-Routers/src/App.js
+++ React-live-coding/React-Routers/src/App.js
@@ -1,19 +1,15 @@
-
-import React, { Component } from 'react'
-import Home from './components/Home'
-import About from './components/About'
-import Users from './components/Users'
-import Contact from './components/Contact'
+import React, { Component } from "react";
+import Home from "./components/Home";
+import About from "./components/About";
+import Users from "./components/Users";
+import Contact from "./components/Contact";
+import { Link, Route } from "react-router-dom";
 
 export default class App extends Component {
-  state={
-    page:"home"
-  }
-
   render() {
     return (
       <div>
-        <ul>
+        {/*   <ul>
           <li onClick={()=>this.setState({page:"home"})}><a href="/">Home</a></li>
           <li onClick={()=>this.setState({page:"about"})}><a href="/about">About</a></li>
           <li onClick={()=>this.setState({page:"users"})}><a href="/users">Users</a></li>
@@ -22,12 +18,41 @@ export default class App extends Component {
         {this.state.page==="home" && <Home/>}
         {this.state.page==="about" && <About/>}
         {this.state.page==="users" && <Users/>}
-        {this.state.page==="contact" && <Contact/>}
+        {this.state.page==="contact" && <Contact/>} */}
+
+        <ul>
+          <li onClick={() => this.setState({ page: "home" })}>
+            <Link to="/">Home</Link>
+          </li>
+          <li onClick={() => this.setState({ page: "about" })}>
+            <Link to="/about">About</Link>
+          </li>
+          <li onClick={() => this.setState({ page: "users" })}>
+            <Link to="/users">Users</Link>
+          </li>
+          <li onClick={() => this.setState({ page: "contact" })}>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+        {/*   <Route exact path="/" component={Home} /> */}
+       {/*  <Route exact path="/" render={()=><Home/>} /> */}
+
+          <Route exact path="/">
+            <Home/>
+          </Route>
 
 
+      {/*     <Route path="/about" component={About} /> */}
+          <Route path="/about">
+            <About/>
+          </Route>
+
+
+          <Route path="/users" component={Users} />
+          <Route path="/contact" component={Contact} />
 
 
       </div>
-    )
+    );
   }
 }
--- React-live-coding/React-Routers/src/components/Contact.js
+++ React-live-coding/React-Routers/src/components/Contact.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default function Contact() {
+export default function Contact(props) {
+    console.log(props)
     return (
         <div>
             <h1>This is our Contact Page</h1>
+            <button onClick={()=>props.history.push("/users")}>go back</button>
         </div>
     )
 }
